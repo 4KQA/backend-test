e008f41f76b9ffecda1b0bd4f84bb9dde60553e2
Nicolaj Schønberg <48961648+Tristan1449@users.noreply.github.com>
2022-09-09 17:17:41 +0200

    added databases connection

--- AKQA-Backend/AKQA-Backend/Helpers/DatabaseContext.cs
+++ AKQA-Backend/AKQA-Backend/Helpers/DatabaseContext.cs
@@ -1,6 +1,23 @@
-﻿namespace AKQA_Backend.Helpers
+﻿using AKQA_Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AKQA_Backend.Helpers
 {
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
     {
+        protected readonly IConfiguration Configuration;
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlServer(Configuration.GetConnectionString("connection"));
+        }
+
+        public DbSet<People> People { get; set; }
     }
 }
--- AKQA-Backend/AKQA-Backend/Program.cs
+++ AKQA-Backend/AKQA-Backend/Program.cs
@@ -1,11 +1,17 @@
+using AKQA_Backend.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddDbContext<DatabaseContext>();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
