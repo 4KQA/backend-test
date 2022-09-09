537cff2441eb5e65947f6b400cf3873ba626acc2
Nicolaj Schønberg <48961648+Tristan1449@users.noreply.github.com>
2022-09-09 17:31:58 +0200

    created function for people

--- AKQA-Backend/AKQA-Backend/Services/People/IPeopleService.cs
+++ AKQA-Backend/AKQA-Backend/Services/People/IPeopleService.cs
@@ -1,6 +1,13 @@
-﻿namespace AKQA_Backend.Services.People
+﻿using AKQA_Backend.Entities;
+using AKQA_Backend.Models;
+
+namespace AKQA_Backend.Services.PeopleService
 {
     public interface IPeopleService
     {
+        void CreatePerson(CreatePeople model);
+        void UpdatePerson(UpdatePeople model);
+        IEnumerable<People> GetAllPeople();
+        People GetPersonByLastName(string lastname);
     }
 }
--- AKQA-Backend/AKQA-Backend/Services/People/PeopleService.cs
+++ AKQA-Backend/AKQA-Backend/Services/People/PeopleService.cs
@@ -1,6 +1,44 @@
-﻿namespace AKQA_Backend.Services.People
+﻿using AKQA_Backend.Entities;
+using AKQA_Backend.Helpers;
+using AKQA_Backend.Models;
+using AutoMapper;
+
+namespace AKQA_Backend.Services.PeopleService
 {
-    public class PeopleService
+    public class PeopleService : IPeopleService
     {
+        private readonly DatabaseContext context;
+        private readonly IMapper mapper;
+
+        public PeopleService(DatabaseContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public void CreatePerson(CreatePeople model)
+        {
+
+        }
+
+        public void UpdatePerson(UpdatePeople model)
+        {
+
+        }
+
+        public IEnumerable<People> GetAllPeople()
+        {
+
+        }
+
+        public People GetPersonByLastName(string lastname)
+        {
+
+        }
+
+        public void GetPercentage()
+        {
+
+        }
     }
 }
