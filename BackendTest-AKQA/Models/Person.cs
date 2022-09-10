8879c1ef8c89be8203ce0dc0e9c2db6face0dd3d
NansyT <nanna.tronbjerg@gmail.com>
2022-09-10 17:10:12 +0200

    Initial commit
    Uploaded project files and created person model

---
+++ BackendTest-AKQA/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendTest_AKQA.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public bool Alive { get; set; }
+    }
+}
