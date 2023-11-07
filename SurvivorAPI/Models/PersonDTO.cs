642badc33e6435cffda248327ac096c853d83fab
rmlajer <eeamalr@students.eaaa.com>
2023-11-07 18:57:57 +0100

    Initial Folder structure and PersonDTO created

---
+++ SurvivorAPI/Models/PersonDTO.cs
@@ -0,0 +1,22 @@
+namespace SurvivorAPI.Models
+{
+    public record PersonDTO
+    {
+
+        public PersonDTO()
+        {
+
+        }
+
+
+        public int? Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+        public string? Gender { get; set; }
+        public double LastLatitude { get; set; }
+        public double LastLongitude { get; set; }
+        public int Status { get; set; }
+
+    }
+}
