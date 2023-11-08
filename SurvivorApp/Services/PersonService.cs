68fc78a8efe5747ef5f61396d8c8778906a438cf
rmlajer <eeamalr@students.eaaa.com>
2023-11-08 15:23:01 +0100

    Blazor Survivor page first draft working

---
+++ SurvivorApp/Services/PersonService.cs
@@ -0,0 +1,37 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using SurvivorApp.Models;
+
+namespace SurvivorApp.Services;
+
+public class PersonService
+{
+    private readonly HttpClient http;
+    private readonly IConfiguration configuration;
+    private readonly string baseAPI = "";
+
+
+    public event Action RefreshRequired;
+
+    public PersonService(HttpClient http, IConfiguration configuration) {
+        this.http = http;
+        this.configuration = configuration;
+        this.baseAPI = configuration["base_api"];
+    }
+
+    public void CallRequestRefresh()
+    {
+         RefreshRequired?.Invoke();
+    }
+
+    public async Task<List<Person>> GetPersons()
+    {
+        string url = $"{baseAPI}persons/";
+        Console.WriteLine("Base API: " + baseAPI);
+        Console.WriteLine("Base URL: " + url);
+        return await http.GetFromJsonAsync<List<Person>>(url);
+    }
+
+    
+}
